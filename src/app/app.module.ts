7d050efbb9351c33631ae8e135e7d725b8ac39cf
SKevinPerera <sumalkevinthushantha@gmail.com>
2020-04-02 19:37:58 +0530

    Initial commit with basic API variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CallingApiService } from './service/calling-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CallingApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { CallingApiService } from './../../service/calling-api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+  
+  //Array of json attributes
+  items = [];
+  
+  /**Need Variables */
+  //Global INFO
+  global_new_cases: string;
+  global_total_cases: string;
+  global_deaths: string;
+  global_recovered: string;
+
+  //Local INFO
+  local_new_cases: string;
+  local_total_cases: string;
+  local_total_number_of_individuals_in_hospitals: string;
+  local_deaths: string;
+  local_recovered: string;
+
+  update_date_time : string;
+
+  constructor(
+    private apiService: CallingApiService,
+  ) { }
+
+  
+
+  ngOnInit() {
+    this.apiService.getInfo()
+    .subscribe
+    (
+      data=>
+      {
+        //Time Equalization
+        this.update_date_time = data.data.update_date_time;
+        
+        //Local INFO
+        this.local_new_cases = data.data.local_new_cases;
+        this.local_total_cases = data.data.local_total_cases;
+        this.local_total_number_of_individuals_in_hospitals = data.data.local_total_number_of_individuals_in_hospitals;
+        this.local_deaths = data.data.local_deaths;
+        this.local_recovered = data.data.local_recovered;
+
+        //Global INFO
+        this.global_new_cases = data.data.global_new_cases;
+        this.global_total_cases = data.data.global_total_cases;
+        this.global_deaths = data.data.global_deaths;
+        this.global_recovered = data.data.global_recovered;
+
+        //Assigning json values to an array
+        for (let key in data){
+          if (data.hasOwnProperty(key)){
+            this.items.push(data[key]);
+          }
+        }
+        console.log(this.items);
+      }
+    );
+  }
+}
